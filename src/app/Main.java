e5c789246f033a057937ad0f6c9f3a44ef755964
Lovelytulip <106096835+Lovelytulip@users.noreply.github.com>
2022-11-11 11:05:08 +0100

    adding methods
    вывод результатов диления и вичетания

--- src/app/Main.java
+++ src/app/Main.java
@@ -11,6 +11,10 @@ public class Main {
         System.out.println(c);
         int d = multiply(a, b);
         System.out.println(d);
+        int e = substract(a, b);
+        System.out.println(e);
+        int f = divide(a, b);
+        System.out.println(f);
     }
 
     private static int add(int a, int b) {
@@ -21,3 +25,18 @@ public class Main {
         return a * b;
     }
 }
+
+    private static int substract(int a, int b) {
+        return a - b;
+    }
+
+    private static int divide(int a, int b) {
+        return a/b;
+    }
+}
+
+
+
+
+
+//2) Добавить функционал вычитания и деления, вывод их результатов (ветка develop).
